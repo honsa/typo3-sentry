b49e88c7afea18934d1523a424fa24bbb9279436
honsa <straka@8038.ch>
2026-03-12 19:42:33 +0100

    Test support v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Honsa',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.7',
+    'version' => '1.0.8',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '13.4.0-14.99.99',
             'php' => '8.2.0-8.4.99',
         ],
         'conflicts' => [],
